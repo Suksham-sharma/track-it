1687f742900bc38151eee7469bb2fc69fe648d5f
{Suksham-sharma} <sukshamever@gmail.com>
2024-12-20 21:53:03 +0530

    prisma setup

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,3 +1,12 @@
-import Router from "express";
+import Router, { Request, Response } from "express";
 
 export const authRouter = Router();
+
+authRouter.post("/login", (req: Request, res: Response) => {
+  try {
+  } catch (error: any) {}
+});
+authRouter.post("/register", (req: Request, res: Response) => {
+  try {
+  } catch (error: any) {}
+});
